7bd5794db717ab6e4985754d6fa08a5b0ba92394
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-26 03:27:17 +0100

    xd

--- reto1-server-express/src/apiRest.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-// Este código pone en marcha al servidor
-app.listen(app.get('port'), function(){
-    console.log(`Server listen on port ${app.get('port')}`)
-});
-
-// En el archivo 'apiRest.js' se introducen las funciones que comienzan con 'app.listen'.
